c8382ec68b1899e67adb00d29e3aa6c60dba84fb
Callan Peter Milne <callanpmilne@gmail.com>
2023-03-29 21:12:37 +1000

    Fix res.render()

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.use((err, req, res, next) => {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ error: err });
 });
 
 module.exports = app;
